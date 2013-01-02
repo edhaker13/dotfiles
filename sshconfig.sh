e5b695dee461e566c548dbd227abb55bd71dd58d
edhaker13 <edhaker13@gmail.com>
2013-01-02 23:52:34 +0000

    More info, real hostname

--- sshconfig.sh
+++ sshconfig.sh
@@ -2,12 +2,12 @@
 # For edhaker.info Git server
 host edhaker
 user git
-hostname edhaker.info
+hostname nemu.edhaker.info
 port 666
 PreferredAuthentications publickey
 identityfile ~/.ssh/id_rsa
 
-# For Checacorp.cz.cc
+# For Checacorp.cz.cc, just in case its the same server really
 host checacorp.cz.cc
 user git
 port 666
