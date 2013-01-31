00ad099154ada74558c6e882de5d6df71d5cdbc2
Luis Checa <edhaker13@gmail.com>
2013-01-31 21:23:56 +0000

    Replaced lighty folder, found old bashrc

---
+++ oldbash.sh
@@ -0,0 +1,592 @@
+# .bashrc
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+[ -z "$PS1" ] && return
+
+# Basic options
+export HISTCONTROL=ignoredups
+export COLORFGBG='default;default'
+# leave some commands out of history log
+export HISTIGNORE="&:??:[ ]*:clear:exit:logout"
+export TIMEFORMAT=$'\nreal %3R\tuser %3U\tsys %3S\tpcpu %P\n'
+export HISTTIMEFORMAT="%H:%M > "
+export HISTIGNORE="&:bg:fg:ll:h"
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(lesspipe)"
+
+#-------------------------------------------------------------
+# Some settings
+#-------------------------------------------------------------
+
+ulimit -S -c 0          # Don't want any coredumps.
+set -o notify
+set -o noclobber
+set -o ignoreeof
+set -o nounset
+#set -o xtrace          # Useful for debuging.
+
+# Enable options:
+shopt -s cdspell
+shopt -s cdable_vars
+shopt -s checkhash
+shopt -s checkwinsize
+shopt -s sourcepath
+shopt -s no_empty_cmd_completion
+shopt -s cmdhist
+shopt -s histappend histreedit histverify
+shopt -s extglob        # Necessary for programmable completion.
+
+# Disable options:
+shopt -u mailwarn
+
+# Aliases
+alias ls='ls -h --color=auto'
+alias ll='ls -l'
+alias la='ls -A'
+alias l='ls -CF'
+alias svim='sudo vim'
+alias h='cd'
+alias ..='cd ..'
+alias cd..='cd ..'
+alias ...='cd ../..'
+alias cim='vim'
+alias back='cd $OLDPWD'
+alias root='sudo su'
+alias su='sudo su'
+alias runlevel='sudo /sbin/init'
+alias grep='grep --color=auto'
+alias dfh='df -h'
+alias yum="sudo yum"
+alias rpm="sudo rpm"
+
+# handy file conversion tools
+alias dos2unix="perl -pi -e 's/\r\n/\n/;'"
+alias unix2dos="perl -pi -e 's/\n/\r\n/;'"
+alias bomstrip="sed -i -s -e '1s/^\xef\xbb\xbf//'"
+
+# Prompt
+NORMAL='\[\033[00m\]'
+BLACK='\[\033[0;30m\]'
+DGRAY='\[033[1;30m\]'
+RED='\[\033[0;31m\]'
+LRED='\[\033[1;31m\]'
+GREEN='\[\033[0;32m\]'
+LGREEN='\[\033[1;32m\]'
+BROWN='\[\033[0;33m\]'
+YELLOW='\[\033[1;33m\]'
+BLUE='\[\033[0;34m\]'
+LBLUE='\[\033[1;34m\]'
+PURPLE='\[\033[0,35m\]'
+LPURPLE='\[\033[1;35m\]'
+CYAN='\[\033[0;36m\]'
+LCYAN='\[\033[1;36m\]'
+LGRAY='\[\033[0;37m\]'
+WHITE='\[\033[1;37m\]'
+PS1="${LPURPLE}\u${LGREEN}@${GREEN}\H${CYAN}[${YELLOW}\w${CYAN}]${LGRAY}>${WHITE}"
+PS2="${GREEN}->"
+PS3="${LPURPLE}*? "
+# Paths
+PATH=$PATH:${HOME}/bin:/usr/lib/wine/bin:/sbin:/usr/sbin
+export PATH=$PATH:/usr/local/bin
+
+# X Terminal titles
+case "$TERM" in
+xterm*|rxvt*)
+	PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD}\007"'
+	;;
+*)
+	;;
+esac
+#-------------------------------------------------------------
+# tailoring 'less'
+#-------------------------------------------------------------
+
+alias more='less'
+export PAGER=less
+export LESSCHARSET='latin1'
+export LESSOPEN='|/usr/bin/lesspipe.sh %s 2>&-'
+   # Use this if lesspipe.sh exists
+export LESS='-i -N -w  -z-4 -g -e -M -X -F -R -P%t?f%f \
+:stdin .?pb%pb\%:?lbLine %lb:?bbByte %bb:-...'
+
+
+#-------------------------------------------------------------
+# spelling typos - highly personnal and keyboard-dependent :-)
+#-------------------------------------------------------------
+
+alias cs='cd'
+alias vf='cd'
+alias moer='more'
+alias moew='more'
+alias kk='ll'
+
+
+#-------------------------------------------------------------
+# A few fun ones
+#-------------------------------------------------------------
+
+
+function xtitle()      # Adds some text in the terminal frame.
+{
+    case "$TERM" in
+        *term | rxvt)
+            echo -n -e "\033]0;$*\007" ;;
+        *)  
+            ;;
+    esac
+}
+
+# aliases that use xtitle
+alias top='xtitle Processes on $HOST && top'
+alias make='xtitle Making $(basename $PWD) ; make'
+alias ncftp="xtitle ncFTP ; ncftp"
+
+# .. and functions
+function man()
+{
+    for i ; do
+        xtitle The $(basename $1|tr -d .[:digit:]) manual
+        command man -F -a "$i"
+    done
+}
+# Functions
+extract () {
+	if [ -f $1 ] ; then
+		case $1 in
+			*.tar.bz2)	tar xjf $1		;;
+			*.tar.gz)	tar xzf $1		;;
+			*.bz2)		bunzip2 $1		;;
+			*.rar)		rar x $1		;;
+			*.gz)		gunzip $1		;;
+			*.tar)		tar xf $1		;;
+			*.tbz2)		tar xjf $1		;;
+			*.tgz)		tar xzf $1		;;
+			*.zip)		unzip $1		;;
+			*.Z)		uncompress $1	;;
+			*)			echo "'$1' cannot be extracted via extract()" ;;
+		esac
+	else
+		echo "'$1' is not a valid file"
+	fi
+}
+ziprm () {
+	if [ -f $1 ] ; then
+		unzip $1
+		rm $1
+	else
+		echo "Need a valid zipfile"
+	fi
+}
+psgrep() {
+	if [ ! -z $1 ] ; then
+		echo "Grepping for processes matching $1..."
+		ps aux | grep $1 | grep -v grep
+	else
+		echo "!! Need name to grep for"
+	fi
+}
+grab() {
+	sudo chown -R ${USER} ${1:-.}
+}
+#-------------------------------------------------------------
+# Process/system related functions:
+#-------------------------------------------------------------
+
+
+function my_ps() { ps $@ -u $USER -o pid,%cpu,%mem,bsdtime,command ; }
+function pp() { my_ps f | awk '!/awk/ && $0~var' var=${1:-".*"} ; }
+
+
+function killps()                 # Kill by process name.
+{
+    local pid pname sig="-TERM"   # Default signal.
+    if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
+        echo "Usage: killps [-SIGNAL] pattern"
+        return;
+    fi
+    if [ $# = 2 ]; then sig=$1 ; fi
+    for pid in $(my_ps| awk '!/awk/ && $0~pat { print $1 }' pat=${!#} ) ; do
+        pname=$(my_ps | awk '$1~var { print $5 }' var=$pid )
+        if ask "Kill process $pid <$pname> with signal $sig?"
+            then kill $sig $pid
+        fi
+    done
+}
+
+function my_ip() # Get IP adresses.
+{
+    MY_IP=$(/sbin/ip addr show venet0 | awk '/inet/ { print $2 } ' | \
+sed -e s/addr://)
+}
+
+function ii()   # Get current host related info.
+{
+    echo -e "\nYou are logged on ${RED}$HOST"
+    echo -e "\nAdditionnal information:$NC " ; uname -a
+    echo -e "\n${RED}Users logged on:$NC " ; w -h
+    echo -e "\n${RED}Current date :$NC " ; date
+    echo -e "\n${RED}Machine stats :$NC " ; uptime
+    echo -e "\n${RED}Memory stats :$NC " ; free
+    my_ip 2>&- ;
+    echo -e "\n${RED}Local IP Address :$NC" ; echo ${MY_IP:-"Not connected"}
+    echo -e "\n${RED}ISP Address :$NC" ; echo ${MY_ISP:-"Not connected"}
+    echo -e "\n${RED}Open connections :$NC "; netstat -pan --inet;
+    echo
+}
+#-------------------------------------------------------------
+# Misc utilities:
+#-------------------------------------------------------------
+# print a fortune cookie
+  if which fortune >/dev/null; then
+      echo -en "\033[0;36m"
+      fortune -a -s | cowsay -f $(ls /usr/share/cowsay/cows/ | shuf | head -n1)
+      echo -en "\033[0;37m"
+  fi
+function repeat()       # Repeat n times command.
+{
+    local i max
+    max=$1; shift;
+    for ((i=1; i <= max ; i++)); do  # --> C-like syntax
+        eval "$@";
+    done
+}
+
+
+function ask()          # See 'killps' for example of use.
+{
+    echo -n "$@" '[y/n] ' ; read ans
+    case "$ans" in
+        y*|Y*) return 0 ;;
+        *) return 1 ;;
+    esac
+}
+
+function corename()   # Get name of app that created a corefile.
+{ 
+    for file ; do
+        echo -n $file : ; gdb --core=$file --batch | head -1
+    done 
+}
+#=========================================================================
+# PROGRAMMABLE COMPLETION - ONLY SINCE BASH-2.04
+# Most are taken from the bash 2.05 documentation and from Ian McDonald's
+# 'Bash completion' package (http://www.caliban.org/bash/#completion).
+# You will in fact need bash more recent than 3.0 for some features.
+#=========================================================================
+if [ -f /etc/bash_completion ]; then
+	. /etc/bash_completion
+fi
+if [ "${BASH_VERSION%.*}" \< "3.0" ]; then
+    echo "You will need to upgrade to version 3.0 \
+for full programmable completion features."
+    return
+fi
+
+shopt -s extglob         # Necessary,
+#set +o nounset          # otherwise some completions will fail.
+
+complete -A hostname   rsh rcp telnet rlogin r ftp ping disk
+complete -A export     printenv
+complete -A variable   export local readonly unset
+complete -A enabled    builtin
+complete -A alias      alias unalias
+complete -A function   function
+complete -A user       su mail finger
+
+complete -A helptopic  help     # Currently, same as builtins.
+complete -A shopt      shopt
+complete -A stopped -P '%' bg
+complete -A job -P '%'     fg jobs disown
+
+complete -A directory  mkdir rmdir
+complete -A directory   -o default cd
+
+# Compression
+complete -f -o default -X '*.+(zip|ZIP)'  zip
+complete -f -o default -X '!*.+(zip|ZIP)' unzip
+complete -f -o default -X '*.+(z|Z)'      compress
+complete -f -o default -X '!*.+(z|Z)'     uncompress
+complete -f -o default -X '*.+(gz|GZ)'    gzip
+complete -f -o default -X '!*.+(gz|GZ)'   gunzip
+complete -f -o default -X '*.+(bz2|BZ2)'  bzip2
+complete -f -o default -X '!*.+(bz2|BZ2)' bunzip2
+complete -f -o default -X '!*.+(zip|ZIP|z|Z|gz|GZ|bz2|BZ2)' extract
+
+
+# Documents - Postscript,pdf,dvi.....
+complete -f -o default -X '!*.+(ps|PS)'  gs ghostview ps2pdf ps2ascii
+complete -f -o default -X '!*.+(dvi|DVI)' dvips dvipdf xdvi dviselect dvitype
+complete -f -o default -X '!*.+(pdf|PDF)' acroread pdf2ps
+complete -f -o default -X \
+'!*.@(@(?(e)ps|?(E)PS|pdf|PDF)?(.gz|.GZ|.bz2|.BZ2|.Z))' gv ggv
+complete -f -o default -X '!*.texi*' makeinfo texi2dvi texi2html texi2pdf
+complete -f -o default -X '!*.tex' tex latex slitex
+complete -f -o default -X '!*.lyx' lyx
+complete -f -o default -X '!*.+(htm*|HTM*)' lynx html2ps
+complete -f -o default -X \
+'!*.+(doc|DOC|xls|XLS|ppt|PPT|sx?|SX?|csv|CSV|od?|OD?|ott|OTT)' soffice
+
+# Multimedia
+complete -f -o default -X \
+'!*.+(gif|GIF|jp*g|JP*G|bmp|BMP|xpm|XPM|png|PNG)' xv gimp ee gqview
+complete -f -o default -X '!*.+(mp3|MP3)' mpg123 mpg321
+complete -f -o default -X '!*.+(ogg|OGG)' ogg123
+complete -f -o default -X \
+'!*.@(mp[23]|MP[23]|ogg|OGG|wav|WAV|pls|m3u|xm|mod|s[3t]m|it|mtm|ult|flac)' xmms
+complete -f -o default -X \
+'!*.@(mp?(e)g|MP?(E)G|wma|avi|AVI|asf|vob|VOB|bin|dat|vcd|\
+ps|pes|fli|viv|rm|ram|yuv|mov|MOV|qt|QT|wmv|mp3|MP3|ogg|OGG|\
+ogm|OGM|mp4|MP4|wav|WAV|asx|ASX)' xine
+
+
+
+complete -f -o default -X '!*.pl'  perl perl5
+
+
+# This is a 'universal' completion function - it works when commands have
+# a so-called 'long options' mode , ie: 'ls --all' instead of 'ls -a'
+# Needs the '-o' option of grep
+#  (try the commented-out version if not available).
+
+# First, remove '=' from completion word separators
+# (this will allow completions like 'ls --color=auto' to work correctly).
+
+COMP_WORDBREAKS=${COMP_WORDBREAKS/=/}
+
+
+_get_longopts() 
+{ 
+    #$1 --help | sed  -e '/--/!d' -e 's/.*--\([^[:space:].,]*\).*/--\1/'| \
+#grep ^"$2" |sort -u ;
+    $1 --help | grep -o -e "--[^[:space:].,]*" | grep -e "$2" |sort -u 
+}
+
+_longopts()
+{
+    local cur
+    cur=${COMP_WORDS[COMP_CWORD]}
+
+    case "${cur:-*}" in
+       -*)      ;;
+        *)      return ;;
+    esac
+
+    case "$1" in
+      \~*)      eval cmd="$1" ;;
+        *)      cmd="$1" ;;
+    esac
+    COMPREPLY=( $(_get_longopts ${1} ${cur} ) )
+}
+complete  -o default -F _longopts configure bash
+complete  -o default -F _longopts wget id info a2ps ls recode
+
+_tar()
+{
+    local cur ext regex tar untar
+
+    COMPREPLY=()
+    cur=${COMP_WORDS[COMP_CWORD]}
+
+    # If we want an option, return the possible long options.
+    case "$cur" in
+        -*)     COMPREPLY=( $(_get_longopts $1 $cur ) ); return 0;;
+    esac
+
+    if [ $COMP_CWORD -eq 1 ]; then
+        COMPREPLY=( $( compgen -W 'c t x u r d A' -- $cur ) )
+        return 0
+    fi
+
+    case "${COMP_WORDS[1]}" in
+        ?(-)c*f)
+            COMPREPLY=( $( compgen -f $cur ) )
+            return 0
+            ;;
+            +([^Izjy])f)
+            ext='tar'
+            regex=$ext
+            ;;
+        *z*f)
+            ext='tar.gz'
+            regex='t\(ar\.\)\(gz\|Z\)'
+            ;;
+        *[Ijy]*f)
+            ext='t?(ar.)bz?(2)'
+            regex='t\(ar\.\)bz2\?'
+            ;;
+        *)
+            COMPREPLY=( $( compgen -f $cur ) )
+            return 0
+            ;;
+
+    esac
+
+    if [[ "$COMP_LINE" == tar*.$ext' '* ]]; then
+        # Complete on files in tar file.
+        #
+        # Get name of tar file from command line.
+        tar=$( echo "$COMP_LINE" | \
+               sed -e 's|^.* \([^ ]*'$regex'\) .*$|\1|' )
+        # Devise how to untar and list it.
+        untar=t${COMP_WORDS[1]//[^Izjyf]/}
+
+        COMPREPLY=( $( compgen -W "$( echo $( tar $untar $tar \
+                    2>/dev/null ) )" -- "$cur" ) )
+        return 0
+
+    else
+        # File completion on relevant files.
+        COMPREPLY=( $( compgen -G $cur\*.$ext ) )
+
+    fi
+
+    return 0
+
+}
+
+complete -F _tar -o default tar
+
+_make()
+{
+    local mdef makef makef_dir="." makef_inc gcmd cur prev i;
+    COMPREPLY=();
+    cur=${COMP_WORDS[COMP_CWORD]};
+    prev=${COMP_WORDS[COMP_CWORD-1]};
+    case "$prev" in
+        -*f)
+            COMPREPLY=($(compgen -f $cur ));
+            return 0
+        ;;
+    esac;
+    case "$cur" in
+        -*)
+            COMPREPLY=($(_get_longopts $1 $cur ));
+            return 0
+        ;;
+    esac;
+
+    # make reads `GNUmakefile', then `makefile', then `Makefile'
+    if [ -f ${makef_dir}/GNUmakefile ]; then
+        makef=${makef_dir}/GNUmakefile
+    elif [ -f ${makef_dir}/makefile ]; then
+        makef=${makef_dir}/makefile
+    elif [ -f ${makef_dir}/Makefile ]; then
+        makef=${makef_dir}/Makefile
+    else
+        makef=${makef_dir}/*.mk        # Local convention.
+    fi
+
+
+    # Before we scan for targets, see if a Makefile name was
+    # specified with -f ...
+    for (( i=0; i < ${#COMP_WORDS[@]}; i++ )); do
+        if [[ ${COMP_WORDS[i]} == -f ]]; then
+           # eval for tilde expansion
+           eval makef=${COMP_WORDS[i+1]}
+           break
+        fi
+    done
+    [ ! -f $makef ] && return 0
+
+    # deal with included Makefiles
+    makef_inc=$( grep -E '^-?include' $makef | \
+    sed -e "s,^.* ,"$makef_dir"/," )
+    for file in $makef_inc; do
+        [ -f $file ] && makef="$makef $file"
+    done
+
+
+    # If we have a partial word to complete, restrict completions to
+    # matches of that word.
+    if [ -n "$cur" ]; then gcmd='grep "^$cur"' ; else gcmd=cat ; fi
+
+    COMPREPLY=( $( awk -F':' '/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ \
+                                {split($1,A,/ /);for(i in A)print A[i]}' \
+                                $makef 2>/dev/null | eval $gcmd  ))
+
+}
+
+complete -F _make -X '+($*|*.[cho])' make gmake pmake
+
+
+_killall()
+{
+    local cur prev
+    COMPREPLY=()
+    cur=${COMP_WORDS[COMP_CWORD]}
+
+    # get a list of processes (the first sed evaluation
+    # takes care of swapped out processes, the second
+    # takes care of getting the basename of the process)
+    COMPREPLY=( $( /usr/bin/ps -u $USER -o comm  | \
+        sed -e '1,1d' -e 's#[]\[]##g' -e 's#^.*/##'| \
+        awk '{if ($0 ~ /^'$cur'/) print $0}' ))
+
+    return 0
+}
+
+complete -F _killall killall killps
+
+# A meta-command completion function for commands like sudo(8), which need to
+# first complete on a command, then complete according to that command's own
+# completion definition - currently not quite foolproof,
+# but still quite useful (By Ian McDonald, modified by me).
+
+_meta_comp()
+{
+    local cur func cline cspec
+
+    COMPREPLY=()
+    cur=${COMP_WORDS[COMP_CWORD]}
+    cmdline=${COMP_WORDS[@]}
+    if [ $COMP_CWORD = 1 ]; then  
+         COMPREPLY=( $( compgen -c $cur ) )
+    else
+        cmd=${COMP_WORDS[1]}            # Find command.
+        cspec=$( complete -p ${cmd} )   # Find spec of that command.
+
+        # COMP_CWORD and COMP_WORDS() are not read-only,
+        # so we can set them before handing off to regular
+        # completion routine:
+        # Get current command line minus initial command,
+        cline="${COMP_LINE#$1 }"
+        # split current command line tokens into array,
+        COMP_WORDS=( $cline )
+        # set current token number to 1 less than now.
+        COMP_CWORD=$(( $COMP_CWORD - 1 ))
+        # If current arg is empty, add it to COMP_WORDS array
+        # (otherwise that information will be lost).
+        if [ -z $cur ]; then COMP_WORDS[COMP_CWORD]=""  ; fi
+
+        if [ "${cspec%%-F *}" != "${cspec}" ]; then
+      # if -F then get function:
+            func=${cspec#*-F }
+            func=${func%% *}
+            eval $func $cline   # Evaluate it.
+        else
+            func=$( echo $cspec | sed -e 's/^complete//' -e 's/[^ ]*$//' )
+            COMPREPLY=( $( eval compgen $func $cur ) )
+        fi
+
+    fi
+    
+}
+
+complete -o default -F _meta_comp nohup \
+eval exec trace truss strace sotruss gdb
+complete -o default -F _meta_comp command type which man nice time
+
+# Locale and editor
+export LANG=en_GB.UTF-8
+export LC_ALL=en_GB.UTF-8
+export EDITOR=nano
+# Server/Users specific aliases and functions
+alias apt-get='sudo apt-get'
+alias dpkg="sudo dpkg"
+alias aptitude='sudo aptitude'
+alias res='screen -dr tty'
+alias sirc='screen -dmS ircs irssi -o Rizon'
+alias irc='screen -rd ircs'
