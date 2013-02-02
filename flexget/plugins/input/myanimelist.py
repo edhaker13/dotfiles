9b422e59470cac9a9522d8562485ee46b348e51a
Luis Checa <edhaker13@gmail.com>
2013-02-02 21:03:07 +0000

    Fix MAL plugin, on_task_entry

--- flexget/plugins/input/myanimelist.py
+++ flexget/plugins/input/myanimelist.py
@@ -15,72 +15,56 @@ class MyAnimeList(object):
        Creates an entry for each item in the current watching anime list.
 
     Syntax:
-        
+
     myanimelist:
       username: <value>
-      
+
     Example:
-        
-      presets:                                                  
-        animeseries:                                            
-          import_series:                                        
-            from:                                               
-              myanimelist:                                      
-                username: 'your username'                         
-            settings:                                           
-              from_group:                                       
-                - Zero-Raws                                     
-                - Leopard-Raws                                  
-              quality: 720p+                                    
-          content_filter:                                       
-            require:                                            
-              - '*.mkv'                                         
-              - '*.mp4'                                         
-          download:                                             
-            'D:\Torrents'                                       
-      feeds:                                                    
-        Nyaa.eu:                                                
-          rss: 'http://www.nyaa.eu/?page=rss&cats=1_11&filter=2'
-          preset:                                               
-            - animeseries                                       
+
+      presets:
+        animeseries:
+          import_series:
+            from:
+              myanimelist:
+                username: 'your username'
 
     Option username are required. Anime list must be public.
     """
-    
+
     def validator(self):
         from flexget import validator
         root = validator.factory('dict')
         root.accept('text', key='username', requried=True)
         return root
-    
+
     @cached('myanimelist', persist='2 hour')
-    def on_feed_input(self, feed, config):
+    def on_task_input(self, feed, config):
         url = 'http://myanimelist.net/animelist/%s&status=1&order=0' % (config['username'])
-        
+
         page = urlopener(url, log)
         soup = get_soup(page)
-        
+
         if soup.find("div", {"class": "badresult"}):
             raise PluginError('Anime list isn\'t public.')
-            
+
         pattern = re.compile('^((http?):\/)?\/?(myanimelist.net)+((\/\w+)*\/)([\w\-\.]+[^#?\s]+)?')
-        
-        entries = []        
+
+        entries = []
         for link in soup.findAll("a", {"class": "animetitle"}):
             if not link.has_key('href'):
                 continue
-            
+
             if not link.contents:
                 continue
-                
+
             match = re.match(pattern, link['href'])
-            
+
             if not match:
                 continue
-            
-            title = match.group(6).replace('_',' ')            
+
+            title = match.group(6).replace('_',' ')
             entries.append(Entry(title, ''))
-            
+
         return entries
 
 register_plugin(MyAnimeList, 'myanimelist', api_ver=2)
