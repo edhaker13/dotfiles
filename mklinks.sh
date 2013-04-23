637b6b63d6a9a88d1d4389dfa1790ae4bbda84cb
edhaker13 <edhaker13@checacorp.com>
2013-04-23 18:04:44 +0100

    Housecleaning, obsolete files, better names

--- mklinks.sh
+++ mklinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles # dotfiles directory
 olddir=~/backup/dotfiles # old dotfiles backup directory
-files="bashrc bash_profile nanorc vimrc screenrc irssi" # list of files/folders to symlink in homedir
+files="bashrc bash_profile flexget nanorc nginx screenrc irssi" # list of files/folders to symlink in homedir
 ##########
 
 # create dotfiles_old in homedir
@@ -24,7 +24,7 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
 echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.$file $olddir
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file.sh ~/.$file
 done
