e24c78191b5064634cf7badb6682f88f2f6ee47e
Luis Checa <edhaker13@gmail.com>
2013-06-12 14:34:49 +0100

    Add colourcolumn

--- vimrc.sh
+++ vimrc.sh
@@ -87,3 +87,9 @@ au FileType c,cpp,java,php,perl,python,html,htmldjango,mason,tt2html,css au BufW
 au BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") | exe "normal g'\"" | endif
 
 set bg=dark                     " set dark background
+" Sets colorcolumn to 80 if exists
+if exists('+colorcolumn')
+    set colorcolumn=80
+else
+    au BufWinEnter * let w:m2=matchadd('ErrorMsg', '\%80v.\+', -1)
+endif
