0950221f46005504892a513458b460911675125d
Luis Checa <edhaker13@gmail.com>
2013-12-06 17:10:06 +0000

    Reorganize and remove home/bin setting

--- bash_profile.sh
+++ bash_profile.sh
@@ -1,18 +1,6 @@
 # Source global definitions
-if [ -f /etc/bashrc ]; then
-    . /etc/bashrc
-fi
-# if running bash
-if [ -n "$BASH_VERSION" ]; then
-    # include .bashrc if it exists
-    if [ -f "$HOME/.bashrc" ]; then
-	. "$HOME/.bashrc"
-    fi
-fi
-
-# set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/bin" ] ; then
-    PATH="$HOME/bin:$PATH"
+if [ -f /etc/profile ]; then
+    . /etc/profile
 fi
 # Looks best on a terminal with black background.....
 echo -e "Welcome $USER... We've been expecting you!"
@@ -22,6 +10,13 @@ if which fortune >/dev/null; then
     echo -en '\033[0;36m'
           fortune -a -s | cowsay -f $(ls /usr/share/cowsay/cows/ | shuf | head -n1)
           echo -en '\033[0;37m'
+fi
+# if running bash
+if [ -n "$BASH_VERSION" ]; then
+    # include .bashrc if it exists
+    if [ -f "$HOME/.bashrc" ]; then
+	. "$HOME/.bashrc"
+    fi
 fi
 #Start SSH Keychain
 #Let re-use ssh-agent and/or gpg-agent between logins
