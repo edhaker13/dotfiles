5e990db2b6417a3c485990115cd915f0d9b5a56f
Luis Checa <luischeca@mail.com>
2015-06-07 01:02:32 +0100

    vim uses spf13-vim

--- init/50_vim.sh
+++ init/50_vim.sh
@@ -2,6 +2,6 @@
 mkdir -p $DOTFILES/caches/vim
 
 # Download Vim plugins.
-if exists vim; then
-  #vim +PlugUpgrade +PlugUpdate +qall
+if hash vim &>/dev/null; then
+  export APP_PAT=H"$DOTFILES/vendor/spf13-vim" && $DOTFILES/vendor/spf13-vim/bootstrap.sh
 fi
