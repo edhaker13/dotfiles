638e368d907a93f4cf883d15a0f2f1a83ae79b48
Luis Checa <edhaker13@gmail.com>
2016-08-20 00:26:09 +0100

    Replaced double quotes with singles

--- bash.d/50_editor.sh
+++ bash.d/50_editor.sh
@@ -7,11 +7,11 @@ else
   export EDITOR='vim'
 fi
 
-export VISUAL="$EDITOR"
-alias se="sudo $EDITOR"
-alias e="$EDITOR"
-alias ev="e $DOTFILES/link/.{,g}vimrc +'cd $DOTFILES'"
-alias es="e $DOTFILES"
+export VISUAL='$EDITOR'
+alias se='sudo $EDITOR'
+alias e='$EDITOR'
+alias ev='e $DOTFILES/link/.{,g}vimrc +"cd $DOTFILES"'
+alias es='e $DOTFILES'
 
 # Stop tilde expansion by bash-completion
 function _expand() { :; }
